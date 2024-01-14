39da2a015da93d3b4efb2a654910d695162414db
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-01-14 07:17:30 -0400

    Se terminaron las cápsulas del 3 al 6 del módulo 1: Elemento TextView

--- EmptyApp/app/src/main/java/com/example/emptyapp/MainActivity.kt
+++ EmptyApp/app/src/main/java/com/example/emptyapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.emptyapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.example.emptyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        //val tvNameDev = findViewById<TextView>(R.id.tvNameDev)
+        val tvNameDev = binding.tvNameDev
+        //tvNameDev.text = getString(R.string.hola_mundo)
+        //tvNameDev.setTextColor(Color.BLUE)
+        //tvNameDev.setOnClickListener {
+        //    Toast.makeText(this,"Haz hecho click en el texto", Toast.LENGTH_SHORT).show()
+        //    tvNameDev.setTextColor(Color.RED)
+        //}
+        tvNameDev.apply {
+            text = getString(R.string.hola_mundo)
+            setTextColor(Color.BLUE)
+            setOnClickListener {
+                Toast.makeText(this.context,"Haz hecho click en el texto", Toast.LENGTH_SHORT).show()
+                tvNameDev.setTextColor(Color.RED)
+            }
+        }
     }
 }
