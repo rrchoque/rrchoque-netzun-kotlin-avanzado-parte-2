63d063e657ce40750f298c85de8763024650f1f7
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-01-18 07:08:25 -0400

    Se terminaron las cápsulas 4,5 y 6 del módulo 2: Chip y ChipGroup

--- EmptyApp/app/src/main/java/com/example/emptyapp/ButtonsActivity.kt
+++ EmptyApp/app/src/main/java/com/example/emptyapp/ButtonsActivity.kt
@@ -2,10 +2,13 @@ package com.example.emptyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.emptyapp.databinding.ActivityButtonsBinding
 import com.example.emptyapp.databinding.ActivityButtonsBinding.*
 import com.example.emptyapp.databinding.ActivityMainBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 private lateinit var binding: ActivityButtonsBinding
 class ButtonsActivity : AppCompatActivity() {
@@ -15,11 +18,50 @@ class ButtonsActivity : AppCompatActivity() {
         binding=ActivityButtonsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var chipGroup = binding.chipGroup
+        var chip: Chip
+
         var button = binding.button
         button.text="Modificado"
 
         button.setOnClickListener {
             Toast.makeText(this,"Haz presionado el boton", Toast.LENGTH_SHORT).show()
+            chipGroup.addChip()
         }
+
+        /**
+         * Ships
+         */
+
+        for (i in 0..<chipGroup.childCount){
+            chip = chipGroup.getChildAt(i) as Chip
+            chip.setOnCloseIconClickListener {
+                chipGroup.removeView(it)
+            }
+
+            chip.setOnClickListener {
+                val temp = it as Chip
+                Toast.makeText(this, "${temp.text} pulsado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun ChipGroup.addChip(){
+        var chip = Chip(this.context)
+        chip.text = "Nuevo Chip"
+        chip.chipIcon = getDrawable(R.drawable.baseline_agriculture_24)
+        chip.isCloseIconVisible= true
+        //chip.style=getDrawable(R.style.chipsEntry)
+
+        chip.setOnCloseIconClickListener {
+            this.removeView(it)
+        }
+
+        chip.setOnClickListener {
+            val temp = it as Chip
+            Toast.makeText(this.context, "${temp.text} pulsado", Toast.LENGTH_SHORT).show()
+        }
+
+        this.addView(chip as View)
     }
 }
